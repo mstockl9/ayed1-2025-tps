dacc4b74e0093475252e922603fdb8c93e50250c
mstockl9 <martinstockl07@gmail.com>
2025-09-06 17:33:53 -0300

    guia 2, ejercicio 8

---
+++ TP2/tp2ej8_impares100y200.py
@@ -0,0 +1,5 @@
+def main():
+    lista_impares = [elem for elem in range(100, 201) if elem % 2]
+    print(f"La lista de todos los impares entre 100 y 200 es la siguiente:\n{lista_impares}")
+
+main()
